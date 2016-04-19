d722e1e27484b9b4b78540981b164ee32ef599ed
Valtu <c.valtulini002@studenti.unibs.it>
2016-04-19 19:47:28 +0200

    Utilità

---
+++ UtilitàProgetto/src/it/unibs/fp/mylib/MyNumeriCasuali.java
@@ -0,0 +1,14 @@
+package it.unibs.fp.mylib;
+
+import java.util.*;
+
+public class MyNumeriCasuali {
+	private static Random estrattore = new Random();
+	
+	public static int estraiIntero(int min, int max){
+		int range = max + 1 - min;
+		int casual = estrattore.nextInt(range);
+		return casual + min;
+	}
+
+}
