e3d2addc75f111a3a8002152fb7f1caab023f98e
Valtu <c.valtulini002@studenti.unibs.it>
2016-04-19 19:47:28 +0200

    Utilità

---
+++ UtilitàProgetto/src/it/unibs/fp/mylib/MyMath.java
@@ -0,0 +1,59 @@
+package it.unibs.fp.mylib;
+
+public class MyMath {
+	
+	public static double arrotondaDoubleNDecimali(double value, int numCifreDecimali) {
+		double temp = Math.pow(10, numCifreDecimali);
+		return Math.round(value * temp) / temp; 
+	}
+	
+	public static int valoreMaxArray(double []array){
+		double valoreMax = array[0];
+		int posizioneMax = 0;
+		
+		for(int i = 0; i < array.length; i++){
+			if(array[i] > valoreMax){
+				valoreMax = array[i];
+				posizioneMax = i;
+			}
+		}
+		
+		return posizioneMax;
+	}
+	
+	public static int valoreMaxArray(double []array, int lunghezzaArray){
+		double valoreMax = array[0];
+		int posizioneMax = 0;
+		
+		for(int i = 0; i < lunghezzaArray; i++){
+			if(array[i] > valoreMax){
+				valoreMax = array[i];
+				posizioneMax = i;
+			}
+		}
+		
+		return posizioneMax;
+	}
+	
+	public static double valoreMedioArray(double []array){
+		double valoreMedio = 0;
+		double somma = 0;
+		
+		for(int i = 0; i < array.length; i++) somma += array[i];
+		
+		valoreMedio = somma/array.length;
+		
+		return valoreMedio;
+	}
+	
+	public static double valoreMedioArray(double []array, int lunghezzaArray){
+		double valoreMedio = 0;
+		double somma = 0;
+		
+		for(int i = 0; i < lunghezzaArray; i++) somma += array[i];
+		
+		valoreMedio = somma/lunghezzaArray;
+		
+		return valoreMedio;
+	}
+}
