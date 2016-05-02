364433d64d34b2a566a53af49bbb44e466bd6926
Giuseppe <gmartinelli024@studenti.unbs.it>
2016-05-02 15:44:41 +0200

    Indentato

--- UtilitàProgetto/src/it/unibs/fp/mylib/MyMenu.java
+++ UtilitàProgetto/src/it/unibs/fp/mylib/MyMenu.java
@@ -6,8 +6,7 @@ e sia presentata in fondo al menu
 */
 package it.unibs.fp.mylib;
 
-class MyMenu
-{
+class MyMenu{
   final private static String CORNICE = "--------------------------------";
   final private static String VOCE_USCITA = "0\tEsci";
   final private static String RICHIESTA_INSERIMENTO = "Digita il numero dell'opzione desiderata > ";
@@ -16,25 +15,21 @@ class MyMenu
   private String [] voci;
 
 	
-  public MyMenu (String titolo, String [] voci)
-  {
+  public MyMenu (String titolo, String [] voci){
 	this.titolo = titolo;
 	this.voci = voci;
   }
 
-  public int scegli ()
-  {
+  public int scegli (){
 	stampaMenu();
 	return MyInput.leggiIntero(RICHIESTA_INSERIMENTO, 0, voci.length);	 
   }
 		
-  public void stampaMenu ()
-  {
+  public void stampaMenu (){
 	System.out.println(CORNICE);
 	System.out.println(titolo);
 	System.out.println(CORNICE);
-    for (int i=0; i<voci.length; i++)
-	 {
+    for (int i=0; i<voci.length; i++){
 	  System.out.println( (i+1) + "\t" + voci[i]);
 	 }
     System.out.println();
