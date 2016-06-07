6ae6a9cdfdf7ce7a25a7f47cdd3973a01c555766
Valtu <Valtu@localhost>
2016-06-07 22:11:31 +0200

    Modificata MyTime, nuovi metodi: creaData, toStringData
    Change-Id: I4b523e6a8443866c345900482c73ebeb3bfa22f6

--- UtilitàProgetto/src/it/unibs/fp/mylib/MyTime.java
+++ UtilitàProgetto/src/it/unibs/fp/mylib/MyTime.java
@@ -1,11 +1,17 @@
 package it.unibs.fp.mylib;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
- * Gestisce i valori relativi al tempo
+ * Gestisce i valori relativi al tempo ed alle date
  * @author Valtulini Claudio
  *
  */
 public class MyTime {
+	private final static String PATTERN_DATA = "dd/MM/yyyy";
+	
 	/**
 	 * Converte i secondi in una stringa nel formato mm:ss
 	 * 
@@ -14,5 +20,20 @@ public class MyTime {
 	 */
 	public static String secondsToString(int seconds) {
 	    return String.format("%02d:%02d", seconds / 60, seconds % 60);
+	}
+	
+	public static Date creaData(String data) throws ParseException{
+		SimpleDateFormat sdf = new SimpleDateFormat();
+		
+		sdf.applyPattern(PATTERN_DATA);
+		Date daRitornare = sdf.parse(data);
+		return daRitornare;
+	}
+	
+	public static String toStringData(Date data){
+		SimpleDateFormat sdf = new SimpleDateFormat();
+		
+		sdf.applyPattern(PATTERN_DATA);
+		return sdf.format(data);
 	}
 }
